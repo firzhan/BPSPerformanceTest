1ad8892be7916fa6cc2624bfd62b22d9762d92a6
firzhan <firzhan007@gmail.com>
2014-07-16 15:18:12 +0530

    12345

---
+++ MathService3/src/main/java/org/wso2/bps/MathService3.java
@@ -0,0 +1,16 @@
+package org.wso2.bps;
+
+public class MathService3{
+	
+	
+	   public int getAddition(int x, int y){
+			
+			return x+y;
+		}
+		
+		public double getSquareRoot(int x){
+			double result =(double) x  * x ;
+			return result ;
+		}
+
+}
---
+++ greetings/src/main/java/org/wso2/bpl/training/GoodMorning.java
@@ -0,0 +1,8 @@
+package org.wso2.bpl.training;
+
+public class GoodMorning{
+	
+		public String greeting(String message){
+			return "GOOD MORNING";
+		}
+}
